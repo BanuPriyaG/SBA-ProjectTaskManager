fb46518209ad45ce98d6d210887f9733ab3d9e09
BanuPriyaG <32223255+BanuPriyaG@users.noreply.github.com>
2018-12-28 00:45:25 +0530

    Update user.ts

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -2,7 +2,7 @@ export class User {
     user_ID:number;
     firstName:String;
     lastName:String;
-    employee_Id:number;
+    employee_ID:number;
     task_Id:number;
     project_Id:number;
 }
